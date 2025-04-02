1926363c2fd5145c1362435b7db37fc9b3e1c00e
Nika Paradina <nika.paradina@gmail.com>
2025-04-02 08:33:49 +0200

    fixed the negative adj_close values

--- select_stocks_with_momentum_strategy.py
+++ select_stocks_with_momentum_strategy.py
@@ -1,12 +1,16 @@
 import sys
 import os
 sys.path.append(os.path.abspath("c:/Users/nikap/Documents/Snpectinatus/"))
-from create_and_clean_dataset import load_data as ld, clear_duplicates as cd, adjust_prices as ap
+import numpy as np
+import pandas as pd
+from create_and_clean_dataset import load_data as ld, clear_duplicates as cd, adjust_prices as ap, remove_prices_below_1e_8 as rpb
 
 # 1. Load data from the CSV file
 prices = ld.load_data() 
 # 2. Remove duplicates
-prices=clear_duplicates = cd.clear_duplicates(prices)
+prices = cd.clear_duplicates(prices)
 # 3. Adjust prices
 ap.adjust_prices(prices)
-print(prices)
+# 4. Filter out rows where any price column is below 1e-8
+prices=rpb.remove_prices_below_1e8(prices)
+# 5. Remove penny stocks
