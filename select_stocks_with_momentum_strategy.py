ab8c6aa4e9139b8d593d57edd958418c9d90679f
Nika Paradina <nika.paradina@gmail.com>
2025-04-07 07:38:24 +0200

    new functions

--- select_stocks_with_momentum_strategy.py
+++ select_stocks_with_momentum_strategy.py
@@ -9,7 +9,10 @@ adjust_prices as ap, \
 remove_prices_below_1e_8 as rpb, \
 remove_penny_stocks as rps,\
 calculate_return_per_symbol_ptc_change as crp, \
-calculate_market_returns_SPY as cmr
+calculate_market_returns_SPY as cmr, \
+clear_stocks_younger_than_a_year as csy, \
+calculate_dollar_value_month as cdvm, \
+calculate_liquidity as cl \
 
 # 1. Load data from the CSV file
 prices = ld.load_data() 
@@ -18,14 +21,23 @@ prices = cd.clear_duplicates(prices)
 # 3. Adjust prices
 ap.adjust_prices(prices)
 # 4. Filter out rows where any price column is below 1e-8
-prices=rpb.remove_prices_below_1e8(prices)
+#prices=rpb.remove_prices_below_1e8(prices)
 # 5. Remove penny stocks (last stock price below 5$)
 prices=rps.remove_penny_stocks(prices)
 # 6. Calculate returns
-print(prices)
 crp.calculate_returns(prices)
+#7. Remove stocks with less than 253 observations (1 year of data). 
+# This is because the momentum will be calculated with T-12 comparison to T-1
+prices=csy.clear_stocks_younger_than_a_year(prices)
+#8. Calculate market returns for SPY
+prices=cmr.calculate_market_returns_SPY(prices)
+#9. Calculate dollar value per month
+prices=cdvm.calculate_dollar_value_month(prices)
+#10.Calculate liquidity
 print(prices)
-#7. Calculate market returns for SPY
-cmr.calculate_market_returns_SPY(prices)
-print("Market ret")
+prices=cl.calculate_liquidity(prices, 1000)
 print(prices)
+
+prices[prices['liquid_1000'] == 1].to_csv(r'C:\Users\nikap\Documents\Snpectinatus\Seasonality results\prices_liquid.csv')
+print(prices[prices['liquid_1000'] == 1])
+print(prices[prices['liquid_1000']['symbol'].unique()])
